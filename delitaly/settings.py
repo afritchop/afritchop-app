62bcc56f388c21aeb7d244e5055074cad3e2af2d
kemdjo <franckmotsebo@gmail.com>
2024-02-17 01:00:37 -0600

    Testing 1

--- delitaly/settings.py
+++ delitaly/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-(!7w)z4zx^^^kbw8n$3@asdadsavgdxg)&bo$(c^p9mqjtaqdsadsadshq3eza)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
